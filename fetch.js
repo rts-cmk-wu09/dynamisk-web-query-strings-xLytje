9705a8842711e727c49743f50e030abe86503f84
xLytje <rasmuslytje@gmail.com>
2023-03-01 09:22:18 +0100

    add html with params

--- fetch.js
+++ fetch.js
@@ -1,5 +1,18 @@
-fetch("./data/destinations.json")
+const destinationslist = document.querySelector('.destinationslist')
+
+fetch('./data/destinations.json')
     .then((response) => response.json())
     .then(data => {
         // console.log(data.destinations[0]);
+        data.destinations.forEach(destination => {
+            destinationslist.innerHTML += `
+            <figure id="${destination.id}">
+            <img src="./img/${destination.image}" alt="">
+            <div>
+            <span><i class="fa-regular fa-heart"></i></span>
+            <a href="/destination.html?id=${destination.id}">More</a>
+            </div>
+            </figure>
+            `;
+        });
     });
