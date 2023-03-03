490a93ffa645325fdf7d03803d9eda16035906d9
xLytje <rasmuslytje@gmail.com>
2023-03-03 08:44:32 +0100

    fix heart toggle + add local storage

--- favorit.js
+++ favorit.js
@@ -1,8 +1,22 @@
-const icons = document.querySelectorAll('.fa-heart');
+// const icons = document.querySelectorAll('.fa-heart');
 
-icons.forEach(icon => {
-  icon.addEventListener('click', () => {
-    icon.classList.toggle('fa-regular');
-    icon.classList.toggle('fa-solid');
-  });
-});
+// icons.forEach(icon => {
+//   icon.addEventListener('click', () => {
+//     icon.classList.toggle('fa-regular');
+//     icon.classList.toggle('fa-solid');
+//   });
+// });
+
+document.querySelector(".destinationslist").addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-heart")) {
+    let destinationsId = e.target.closest("figure").attributes.id.value;
+    if (!localStorage.getItem(destinationsId, destinationsId)) {
+      localStorage.setItem(destinationsId, destinationsId);
+      e.target.classList.add("fa-solid");
+    } else {
+      localStorage.removeItem(destinationsId);
+      e.target.classList.remove("fa-solid");
+      e.target.classList.add("fa-regular");
+    }
+  }
+})
